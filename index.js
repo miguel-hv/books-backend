d57519711c1e708841a7e797c421cc461ff6b6f0
mhdelavega <huertadelavegamiguel@gmail.com>
2022-04-30 10:10:51 +0200

    FEAT: cors

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const Author = require('./models/Author.model');
 const db = require('./db.js');
+const cors = require('cors');
 
-const PORT = 3000;
+const PORT = 3001;
 db.connect();
 const app = express();
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+app.use(cors({
+  origin: ['http://localhost:3000'], //todo: add heroku name,
+  credentials: true, 
+}));
 
 const router = express.Router();
 const authorsRoutes = require('./routes/authors.routes');

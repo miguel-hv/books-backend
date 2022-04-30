3b304591bb82e94b631f12e94e9b629731b7b2b7
mhdelavega <huertadelavegamiguel@gmail.com>
2022-04-30 16:33:36 +0200

    FIX:books get

--- controllers/books.controller.js
+++ controllers/books.controller.js
@@ -4,7 +4,7 @@ const Book = require("../models/Book.model");
 const booksGet = async (req, res) => {
 
 	try {
-		const books = await Book.find();
+		const books = await Book.find().populate('author');
 		return res.status(200).json(books);
 
 	} catch (err) {
